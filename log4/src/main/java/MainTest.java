e23dde0cb81a609d3e45fa0e1f73e4531274dd69
Jeff Gong <bernkafly@gmail.com>
2017-08-01 18:53:16 +0800

    Add files via upload

---
+++ log4/src/main/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.apache.log4j.Logger;
+public class MainTest {
+
+    private static Logger logger = Logger.getLogger(MainTest.class);
+    public static void main(String[] args){
+
+
+        logger.debug("this is a debug");
+        logger.info("this is a info");
+    }
+
+
+}
+
